5819ce9bf59790c57c821071750355474a809d29
Marco Pavez <marcopz03@outlook.com>
2024-04-27 22:16:58 -0400

    add: metodo para registrar nuevos errores

--- responser/responseModel.go
+++ responser/responseModel.go
@@ -1,39 +1,27 @@
 package responser
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type CustomResponseCreator interface {
-	// Para implementar la interfaz de error
 	Error() string
-	// Getter del codigo de estatus http
 	GetStatusCode() int
-	// Getter del codigo interno definido por la empresa
-	GetInternalCode() string
-	// getter del mensaje personalizado de la respuesta
 	GetResponseMessage() string
 }
 
-// Estructura que implementa mensajes personalizados para responder
-// las request HTTP cuando ocurre un error.
 type HTTPResponse struct {
-	// Error arrojado por el proceso del endpoint
-	cause error
-	// Mensaje custom relacionado al error
-	details string
-	// Codigo de estado de la respuesta
+	cause      error
+	details    string
 	statusCode int
-	// Codigo interno de la respuesta
-	internalCode string
-	// Indica el archivo y línea de código desde la cual se invoca a la funcion de error
-	location string
+	location   string
 }
 
-func HTTPErrorInstance(e error, httpStatusCode int, tannerInternalCode string) *HTTPResponse {
+func NewHttpResponse(e error, httpStatusCode int) *HTTPResponse {
 	return &HTTPResponse{
-		cause:        e,
-		statusCode:   httpStatusCode,
-		internalCode: tannerInternalCode,
-		location:     runtimeToString(),
+		cause:      e,
+		statusCode: httpStatusCode,
+		location:   runtimeToString(),
 	}
 }
 
@@ -46,10 +34,6 @@ func (err HTTPResponse) Error() string {
 
 func (err HTTPResponse) GetStatusCode() int {
 	return err.statusCode
-}
-
-func (err HTTPResponse) GetInternalCode() string {
-	return err.internalCode
 }
 
 func (err HTTPResponse) GetResponseMessage() string {
